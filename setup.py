ecf81840d7e76e03ba78d7099da92f02785f4860
Stephane Thiell <stephane.thiell@cea.fr>
2009-04-08 13:27:04 +0000

    * add setup.py
    This script which uses distutils allow python installation of shine package.
    
    See mkrelease.sh which makes use of this script.
    
    SVN commit: r114

---
+++ setup.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+#
+# Copyright (C) 2007, 2008, 2009 CEA
+#
+# This file is part of shine
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+# $Id$
+
+from distutils.core import setup
+import os
+
+setup(name='shine',
+      version=os.environ['SHINEVERSION'],
+      license='GPL',
+      description='Lustre shine administration utility',
+      author='Stephane Thiell',
+      author_email='stephane.thiell@cea.fr',
+      url='http://lustre-shine.sourceforge.net/',
+      package_dir={'': 'lib'},
+      packages=['Shine',
+               'Shine.Commands',
+               'Shine.Commands.Base',
+               'Shine.Commands.Base.Support',
+               'Shine.Configuration',
+               'Shine.Configuration.Backend',
+               'Shine.Configuration.Backend.FileSupport',
+               'Shine.Lustre',
+               'Shine.Lustre.Actions',
+               'Shine.Lustre.Actions.Proxies',
+               'Shine.Utilities'],
+      data_files=[('/usr/sbin', ['scripts/shine']),
+                  ('/var/cache/shine/conf', ['conf/cache/README']),
+                  ('/usr/share/vim/vim70/syntax', ['doc/extras/vim/syntax/shine.vim']),
+                  ('/usr/share/vim/vim70/syntax', ['doc/extras/vim/syntax/shinefs.vim']),
+                  ('/usr/share/vim/vim70/ftdetect', ['doc/extras/vim/ftdetect/shine.vim']),
+                  ('/usr/share/shine', ['scripts/shine.init.redhat'])]
+     )
+
