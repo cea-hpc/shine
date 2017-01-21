f019ab41c1d9fba05d2221863c86f6783b2e06ae
Stephane Thiell <sthiell@stanford.edu>
2017-01-21 12:19:35 -0800

    New target status: "no_device"
    Add a new target status: no_device when the target device is not
    available on the host.
    
    A missing device is not always an error, like with mdadm.
    
    Change-Id: Ifd402c6b5b4f827995e5bc664a1a5ab3c8fe8ebf

--- lib/Shine/Configuration/ModelFile.py
+++ lib/Shine/Configuration/ModelFile.py
@@ -241,10 +241,11 @@ class MultipleElement(object):
     it will have to manage.
     """
 
-    def __init__(self, orig_elem, fold=False):
+    def __init__(self, orig_elem, fold=False, enable_ranges=True):
         self._origelem = orig_elem
         self._elements = []
         self.fold = fold
+        self._enable_ranges = enable_ranges
 
     def emptycopy(self):
         """Return a new empty copy of this element, with the same attributes."""
@@ -458,7 +459,12 @@ class MultipleElement(object):
 
     def parse(self, data):
         """Add a new element to this MultipleElement and call parse() on it."""
-        for data in self._expand_range(data):
+        if self._enable_ranges:
+            lines = self._expand_range(data)
+        else:
+            lines = [data]
+
+        for data in lines:
             newone = self._origelem.emptycopy()
             newone.parse(data)
             self._elements.append(newone)
@@ -535,7 +541,8 @@ class ModelFile(object):
         """
         self.add_custom(name, SimpleElement(**kwargs), multiple, fold)
 
-    def add_custom(self, name, custom_elem, multiple=False, fold=False):
+    def add_custom(self, name, custom_elem, multiple=False, fold=False,
+                   expand_range=True):
         """Add a custom Element type.
 
         This could be any element that supports needed interface.
@@ -544,7 +551,8 @@ class ModelFile(object):
             raise KeyError("%s is already declared" % name)
 
         if multiple:
-            self._elements[name] = MultipleElement(custom_elem, fold=fold)
+            self._elements[name] = MultipleElement(custom_elem, fold=fold,
+                                                   enable_ranges=expand_range)
         else:
             self._elements[name] = custom_elem
 
