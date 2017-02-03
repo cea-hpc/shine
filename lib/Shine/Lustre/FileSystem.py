32c7997a2f4cb7ac6a13b8dceece3fdbde13661d
Stephane Thiell <sthiell@stanford.edu>
2017-02-02 16:26:08 -0800

    Lustre.FileSystem: fix dynamic import of targets
    Fix current code that doesn't work when subclassing FileSystem.
    
    Change-Id: I11f01233c75de56fe41ae77e10765d29272c0957

--- lib/Shine/Lustre/FileSystem.py
+++ lib/Shine/Lustre/FileSystem.py
@@ -222,11 +222,11 @@ class FileSystem:
             raise FSStructureError("A Lustre FS has only one MGT.")
 
         # Instantiate matching target class (eg. 'ost' -> OST).
-        module_name = sys.modules[self.__class__.__module__]
-        target = getattr(module_name, type.upper())(fs=self, server=server,
+        mod = __import__('Shine.Lustre.Target', fromlist=[None])
+        target = getattr(mod, type.upper())(fs=self, server=server,
                 index=index, dev=dev, jdev=jdev, group=group, tag=tag,
                 enabled=enabled, mode=mode, network=network, active=active)
-        
+
         self._attach_component(target)
 
         return target
