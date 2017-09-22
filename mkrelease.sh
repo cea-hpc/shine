25b792a2df1abab3157e4efc64234b029b345196
Aurelien Degremont <aurelien.degremont@cea.fr>
2017-09-22 22:26:39 +0200

    packaging: lots of cleaning of shine distribution
    Use setuptool instead of distutils:
    - add an entry_point for Shine.Controller and use it
      in console_scripts instead of a specific script
    - improve specfile and mkrelease.sh accordingly
    - improve metadata in setup.py
    
    Update metadata related to SourceForge migration to GitHub
    in various places.
    
    Change-Id: I9467dd6b5c4dc3bbf84d948454eb61ccf2346a5f

--- mkrelease.sh
+++ mkrelease.sh
@@ -24,27 +24,21 @@ if [ `grep -c "^public_version = \"$version\"" $VERSIONFILE` -eq 0 ]; then
     echo "Version doesn't match $VERSIONFILE:"
     echo
     cat $VERSIONFILE
-    echo -n "Continue (y/N)? "
-    read cont
-    if [ ! x$cont = xy -a ! x$cont = xY ]; then
-        exit 1
-    fi
+    exit 1
 fi
 
 clean
 
 # build a source distribution
-export SHINEVERSION=$version
 python setup.py sdist || exit 1
 
 # build RPMs
 mkdir -p $TOPDIR/BUILD
 rpmbuild -ba \
     --define "_topdir $TOPDIR" \
-    --define "version $SHINEVERSION" \
     --define "_rpmdir $PWD/dist" \
     --define "_srcrpmdir $PWD/dist" \
     --define "_sourcedir $PWD/dist" \
-    --clean dist/shine.spec
+    --clean shine.spec
 
 clean
