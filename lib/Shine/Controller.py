cea-hpc/shine

commit b398a56a54b038b28fcfd512bb9f0fa330a8de4f

packaging: lots of cleaning of shine distribution
Use setuptool instead of distutils:
- add an entry_point for Shine.Controller and use it
  in console_scripts instead of a specific script
- improve specfile and mkrelease.sh accordingly
- improve metadata in setup.py

Update metadata related to SourceForge migration to GitHub
in various places.

Change-Id: I9467dd6b5c4dc3bbf84d948454eb61ccf2346a5f

lib/Shine/Controller.py
@@ -287,3 +287,6 @@ class Controller(object):
             pass
 
         return rc
+
+if __name__ == '__main__':
+    Controller().run_command()

setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Copyright (C) 2007-2013 CEA
+# Copyright (C) 2007-2017 CEA
 #
 # This file is part of shine
 #
@@ -19,30 +19,45 @@
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #
 
-from distutils.core import setup
-import os
+from setuptools import setup, find_packages
+
+VERSION='1.5'
 
 setup(name='shine',
-      version=os.environ['SHINEVERSION'],
-      license='GPL',
+      version=VERSION,
+      license='GPLv2+',
       description='Lustre administration utility',
-      author='Stephane Thiell',
-      author_email='stephane.thiell@cea.fr',
-      url='http://lustre-shine.sourceforge.net/',
+      author='Aurelien Degremont',
+      author_email='aurelien.degremont@cea.fr',
+      url='https://github.com/cea-hpc/shine',
+      download_url='https://github.com/cea-hpc/shine/releases/v%s' % VERSION,
       package_dir={'': 'lib'},
-      packages=['Shine',
-               'Shine.CLI',
-               'Shine.Commands',
-               'Shine.Commands.Base',
-               'Shine.Configuration',
-               'Shine.Configuration.Backend',
-               'Shine.Lustre',
-               'Shine.Lustre.Actions'],
-      data_files=[('/usr/sbin', ['scripts/shine']),
+      packages=find_packages('lib'),
+      data_files=[('/etc/shine',
+                   ['conf/shine.conf',
+                    'conf/storage.conf',
+                    'conf/tuning.conf.example']),
+                  ('/etc/shine/models', ['conf/models/example.lmf']),
                   ('/var/cache/shine/conf', ['conf/cache/README']),
-                  ('/usr/share/vim/vim70/syntax', ['doc/extras/vim/syntax/shine.vim']),
-                  ('/usr/share/vim/vim70/syntax', ['doc/extras/vim/syntax/shinefs.vim']),
-                  ('/usr/share/vim/vim70/ftdetect', ['doc/extras/vim/ftdetect/shine.vim']),
-                  ('/usr/share/shine', ['scripts/shine.init.redhat'])]
+                  ('/usr/share/vim/vimfiles/syntax',
+                   ['doc/extras/vim/syntax/shine.vim',
+                    'doc/extras/vim/syntax/shinefs.vim']),
+                  ('/usr/share/vim/vimfiles/ftdetect',
+                   ['doc/extras/vim/ftdetect/shine.vim']),
+                  ('/usr/share/shine', ['scripts/shine.init.redhat'])],
+      entry_points={'console_scripts': ['shine=Shine.Controller:main']},
+      classifiers=[
+          "Development Status :: 5 - Production/Stable",
+          "Environment :: Console",
+          "Intended Audience :: System Administrators",
+          "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
+          "Operating System :: POSIX :: Linux",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2.4",
+          "Programming Language :: Python :: 2.5",
+          "Programming Language :: Python :: 2.6",
+          "Programming Language :: Python :: 2.7",
+          "Topic :: System :: Clustering",
+          "Topic :: System :: Distributed Computing"
+      ],
      )
-

.gitignore
@@ -1,3 +1,11 @@
 .gitignore
+
+# Compiled python modules.
 *.pyc
 
+# Setuptools distribution folder.
+/dist/
+
+# Python egg metadata, regenerated from source files by setuptools.
+/lib/*.egg-info
+/lib/*.egg

MANIFEST.in
@@ -6,7 +6,6 @@ include conf/shine.conf
 include conf/storage.conf
 include conf/tuning.conf.example
 include conf/cache/README
-include scripts/shine
 include scripts/shine.init.redhat
 include doc/shine.1
 include doc/shine.conf.5

debian/control
@@ -5,13 +5,13 @@ Maintainer: Arnaud Guignard <arnaud.guignard@cea.fr>
 Build-Depends: debhelper (>= 9),python (>= 2.6.6-3), python-setuptools (>= 0.6)
 X-Python-Version: >= 2.4
 Standards-Version: 3.9.5
-Vcs-Git: http://git.code.sf.net/p/lustre-shine/git
-Vcs-Browser: http://sourceforge.net/p/lustre-shine/git/ci/master/tree/
+Vcs-Git: http://github.com/cea-hpc/shine.git
+Vcs-Browser: https://github.com/cea-hpc/shine/commits/master
 
 Package: shine
 Architecture: all
 Depends:  ${python:Depends}, ${misc:Depends}, clustershell
 Recommends: vim-addon-manager
 Description: Lustre file system administration utility
- Command line tools designed to setup and manage a Lustre file system on a 
+ Command line tools designed to setup and manage a Lustre file system on a
  cluster.

debian/copyright
@@ -1,9 +1,9 @@
 Format: http://www.debian.org/doc/packaging-manuals/copyright-format/1.0/
 Upstream-Name: shine
-Source: http://lustre-shine.sourceforge.net/
+Source: https://github.com/cea-hpc/shine
 
 Files: *
-Copyright: 2008-2015 Commissariat à l'Energie Atomique et aux Energies Alternatives (CEA)
+Copyright: 2008-2017 Commissariat à l'Energie Atomique et aux Energies Alternatives (CEA)
 Authors: Stephane Thiell <stephane.thiell@cea.fr>
          Aurelien Degremont  <aurelien.degremont@cea.fr>
 License: GPL-2+

debian/rules
@@ -1,9 +1,8 @@
 #!/usr/bin/make -f
-export SHINEVERSION=$(VERSION)
 
 override_dh_auto_install:
 	dh_auto_install
-	rm -rf debian/shine/usr/share/vim/vim70/
+	rm -rf debian/shine/usr/share/vim/vimfiles/
 	rm -rf debian/shine/usr/share/shine/
 	rm -f  debian/shine/var/cache/shine/conf/README
 

dist/shine.spec
@@ -1,59 +1,0 @@
-
-Name: shine
-Summary: Lustre administration utility
-Version: %{version}
-Release: 1%{?dist}
-Source0: %{name}-%{version}.tar.gz
-License: GPL
-Group: Development/Libraries
-BuildRoot: %{_tmppath}/%{name}-buildroot
-Prefix: %{_prefix}
-BuildArchitectures: noarch
-Requires: clustershell >= 1.5.1
-Vendor: CEA
-Url: http://lustre-shine.sourceforge.net/
-
-%description
-Lustre administration utility.
-
-%prep
-%setup
-
-%build
-python setup.py build
-
-%install
-python setup.py install --root=$RPM_BUILD_ROOT --record=INSTALLED_FILES
-mkdir -p $RPM_BUILD_ROOT/%{_sysconfdir}/shine/models
-cp conf/*.conf* $RPM_BUILD_ROOT/%{_sysconfdir}/shine
-cp conf/models/* $RPM_BUILD_ROOT/%{_sysconfdir}/shine/models
-# man pages
-mkdir -p $RPM_BUILD_ROOT/%{_mandir}/{man1,man5}
-gzip -c doc/shine.1 >$RPM_BUILD_ROOT/%{_mandir}/man1/shine.1.gz
-gzip -c doc/shine.conf.5 >$RPM_BUILD_ROOT/%{_mandir}/man5/shine.conf.5.gz
-
-%clean
-rm -rf $RPM_BUILD_ROOT
-
-%files -f INSTALLED_FILES
-%defattr(-,root,root)
-
-%config(noreplace) %{_sysconfdir}/shine/*.conf
-%config %{_sysconfdir}/shine/*.conf.example
-%config %{_sysconfdir}/shine/models/*.lmf
-%doc LICENSE README.md ChangeLog
-%doc %{_mandir}/man1/shine.1.gz
-%doc %{_mandir}/man5/shine.conf.5.gz
-
-%changelog
-* Wed May 24 2017 <aurelien.degremont@cea.fr> - 1.5-1
-- Update to shine 1.5
-
-* Wed Apr 29 2015 <aurelien.degremont@cea.fr> - 1.4-1
-- Update to shine 1.4
-
-* Tue Mar 11 2014 <aurelien.degremont@cea.fr> - 1.3.1-1
-- Update to shine 1.3.1
-
-* Thu Oct 10 2013 <aurelien.degremont@cea.fr> - 1.3-1
-- Update to shine 1.3

commit 880d657ecb5e66d7139b3c9ea72c4315a317fe4d

Controller: define an actual function for entry point
The automagic setuptools entry_points console_scripts need to
call a real function to work, the current wrappers fail with:
ImportError: <module 'Shine.Controller' from '/usr/lib/python2.7/site-packages/Shine/Controller.pyc'> has no 'main' attribute


Fixes: b398a56a54b0 ("packaging: lots of cleaning of shine distribution")
Change-Id: I6886313e1026bbfc018fa068d8be5bad4727624e

lib/Shine/Controller.py
@@ -288,5 +288,8 @@ class Controller(object):
 
         return rc
 
-if __name__ == '__main__':
+def run():
     Controller().run_command()
+
+if __name__ == '__main__':
+    run()

setup.py
@@ -45,7 +45,7 @@ setup(name='shine',
                   ('/usr/share/vim/vimfiles/ftdetect',
                    ['doc/extras/vim/ftdetect/shine.vim']),
                   ('/usr/share/shine', ['scripts/shine.init.redhat'])],
-      entry_points={'console_scripts': ['shine=Shine.Controller:main']},
+      entry_points={'console_scripts': ['shine=Shine.Controller:run']},
       classifiers=[
           "Development Status :: 5 - Production/Stable",
           "Environment :: Console",